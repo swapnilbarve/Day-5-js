a60074cb0495994ce47c40a5d21cec8107332ae3
swapnilbarve <100339918+swapnilbarve@users.noreply.github.com>
2022-04-27 20:20:26 +0530

    Add files via upload

---
+++ Day5.css
@@ -0,0 +1,60 @@
+console.log("Hello Javascript");
+
+
+
+//for loop
+
+
+ for(let i = 0;i < 10;i++){
+    console.log(i);
+} 
+
+
+
+ for(let i = 10;i < 20;i++){
+    console.log(i);
+}
+
+let a = 10;
+while(a>0){
+    console.log(a);
+    a--;
+} 
+
+
+for(let k = 1;k <= 10;k++){
+    console.log(k*2);
+}
+
+
+let b = 1;
+while( b <= 10){
+    console.log(b*2);
+    b++;
+}
+
+let c = 2;
+do{
+    console.log(c);
+    c=c+2;
+}while (c <= 20);
+
+let tourist = ["swapnil","ankit","swarup","Sid"];
+for(let key in tourist){
+    console.log(key);
+}
+
+let tourist = ["swapnil","ankit","swarup","Sid"];
+for(let values of tourist){
+    console.log(key);
+}
+
+
+let tourist = ["swapnil","ankit","swarup","Sid"];
+tourist.forEach((values,index))=>{
+    console.log(index,values)
+}
+
+
+
+
